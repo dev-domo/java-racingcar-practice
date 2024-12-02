12bb9cad645114a9b9702d6188bc4d41831064ac
pakaheo <k70923@konkuk.ac.kr>
2024-12-02 22:50:02 +0900

    test : Car 전진 횟수 비교 및 getName 테스트

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -3,6 +3,8 @@ package racingcar.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class CarTest {
     @Test
@@ -17,5 +19,25 @@ public class CarTest {
         Car car = new Car("pobi");
         car.move(3);
         assertThat(car).isEqualTo(new Car("pobi", 0));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"3:4", "5:5"}, delimiter = ':')
+    void 전진_횟수_최댓값_찾기(int position, int maxForward) {
+        Car car = new Car("pobi", 4);
+        assertThat(car.calculateMaxForward(position)).isEqualTo(maxForward);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"4:true", "5:false"}, delimiter = ':')
+    void 같은_전진_횟수인지_판단하기(int position, boolean expected) {
+        Car car = new Car("pobi", 4);
+        assertThat(car.isSamePosition(position)).isEqualTo(expected);
+    }
+
+    @Test
+    void 자동차_이름_가져오기() {
+        Car car = new Car("pobi");
+        assertThat(car.getName()).isEqualTo("pobi");
     }
 }
