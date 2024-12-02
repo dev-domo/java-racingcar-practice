cd81c29a8e8a2f2f3e6d8621f65f91b372abc71e
pakaheo <k70923@konkuk.ac.kr>
2024-12-02 20:44:23 +0900

    feat : 자동차는 값이 4 이상이면 전진

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,2 +1,38 @@
-package racingcar.view;public class Car {
+package racingcar.domain;
+
+import java.util.Objects;
+
+public class Car {
+    private final String name;
+    private int position;
+
+    public Car(final String name) {
+        this(name, 0);
+    }
+
+    public Car(final String name, int position) {
+        this.name = name;
+        this.position = position;
+    }
+
+    public Car move(int step) {
+        position += step;
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (!(object instanceof Car car)) {
+            return false;
+        }
+        return position == car.position && Objects.equals(name, car.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, position);
+    }
 }
--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -1,2 +1,14 @@
-package racingcar.domain;public class CarTest {
+package racingcar.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class CarTest {
+    @Test
+    void 랜덤값이_4이상이면_자동차는_전진한다() {
+        Car car = new Car("pobi");
+        car.move(4);
+        assertThat(car).isEqualTo(new Car("pobi", 4));
+    }
 }
