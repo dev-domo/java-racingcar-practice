3f672a96d7308796e2dbd099dc1154610aa7fbba
pakaheo <k70923@konkuk.ac.kr>
2024-12-02 20:16:36 +0900

    feat : 시도 횟수 입력 문구 출력 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -6,4 +6,8 @@ public class OutputView {
     public void promptForInputCarNames() {
         System.out.println(OutputMessage.INPUT_CAR_NAMES.valueOf());
     }
+
+    public void promptForInputTryCount() {
+        System.out.println(OutputMessage.INPUT_TRY_COUNT.valueOf());
+    }
 }
