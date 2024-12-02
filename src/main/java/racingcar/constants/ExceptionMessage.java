697da29764e5f11b6d3932b02d08702a6c5e20cb
pakaheo <k70923@konkuk.ac.kr>
2024-12-02 23:06:31 +0900

    feat : 에러 메시지 기능 구현

--- src/main/java/racingcar/constants/ExceptionMessage.java
+++ src/main/java/racingcar/constants/ExceptionMessage.java
@@ -1,2 +1,16 @@
-package racingcar.constants;public class ExceptionMessage {
+package racingcar.constants;
+
+public enum ExceptionMessage {
+    INVALID_CAR_NAME_LENGTH("[ERROR] 자동차 이름은 5자 이하여야 합니다."),
+    UNDIVIDED_BY_SEPARATOR("[ERROR] 자동차 이름은 쉼표로 구분되어야 합니다.");
+
+    private final String message;
+
+    ExceptionMessage(final String message) {
+        this.message = message;
+    }
+
+    public String valueOf() {
+        return message;
+    }
 }
